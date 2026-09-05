0b2a75844e37d408b6e19dbe888b3e7670666316
Pratik <prateekchapagain643@gmail.com>
2026-09-05 19:01:52 +0545

    stage 2: breaking taken input into seprate arguments

--- myshell.c
+++ myshell.c
@@ -2,20 +2,45 @@
 #include<string.h>
 int main(){
 char input[100];
-int i;
+int i=0,j=0;
+//Shell continous running loop
 do{
 printf("myshell>");
 fgets(input,sizeof(input),stdin);
 for(i=0;i<strlen(input);i++){
+// trimming out the spaces after words
 if(input[i]=='\n'){
-int j = i;
+j = i;
 while(input[j-1]==' '){
 j--;
 }
 input[j]='\0';
 }
 }
-printf("%s\n",input);
+//Seprating the arguments
+char *token[10];
+i=0;
+j=0;
+token[j]=&input[i];
+while(input[i]!='\0'){
+if(input[i]==' '){
+input[i]='\0';
+j++;
+i++;//so that k won't have to check from exact index where we ended
+int k = i;
+while(input[k]==' '){
+k++;
+}
+token[j]=&input[k];
+i=k;
+continue;
+}
+i++;
+}
+//Displaying the arguments
+int count = j;
+for(j=0;j<=count;j++){
+printf("%s\n",token[j]);}
 }while(strcmp(input,"exit"));
 return 0;
 }
